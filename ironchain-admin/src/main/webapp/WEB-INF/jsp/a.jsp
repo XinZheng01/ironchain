4f8d2a5dd732be2994f8fa99279c3de0db81cdf9
XinZheng01 <362117444@qq.com>
2017-03-20 19:27:10 +0800

    2017-3-20-2

--- ironchain-admin/src/main/webapp/WEB-INF/jsp/a.jsp
+++ ironchain-admin/src/main/webapp/WEB-INF/jsp/a.jsp
@@ -173,34 +173,7 @@
 <script src="${staticUrl}/plugins/datatables-1.10.13/media/js/dataTables.zui.js"></script>
 <script src="${staticUrl}/plugins/zui-1.6.0/lib/datetimepicker/datetimepicker.min.js"></script>
 <script type="text/javascript">
-var table = $('#example').DataTable({
-	pagingType:   "full_numbers",
-	sLoadingRecords: "正在加载数据...",
-	sZeroRecords: "暂无数据",
-	stateSave: true,
-	searching: false,
-	dom: 'rt<"bottom"iflp<"clear">>',
-	language: {
-        "processing": "玩命加载中...",
-		"lengthMenu": "显示 _MENU_ 项结果",
-		"zeroRecords": "没有匹配结果",
-		"info": "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
-		"infoEmpty": "显示第 0 至 0 项结果，共 0 项",
-		"infoFiltered": "(由 _MAX_ 项结果过滤)",
-		"infoPostFix": "",
-		"url": "",
-		"paginate": {
-			"first":    "首页",
-			"previous": "上一页",
-			"next":     "下一页",
-			"last":     "末页"
-		}
-   },
-   _fnPageChange:function(){
-   	alert("1111");
-   }
-});
-
+var table = $('#example').DataTable();
 $(".form-datetime").datetimepicker(
 {
     weekStart: 1,
--- ironchain-admin/src/main/webapp/static/plugins/datatables-1.10.13/media/js/dataTables.zui.js
+++ ironchain-admin/src/main/webapp/static/plugins/datatables-1.10.13/media/js/dataTables.zui.js
@@ -42,14 +42,69 @@
 'use strict';
 var DataTable = $.fn.dataTable;
 
-
 /* Set the defaults for DataTables initialisation */
 $.extend( true, DataTable.defaults, {
 	dom:
-		"<'row'<'col-sm-6'l><'col-sm-6'f>>" +
-		"<'row'<'col-sm-12'tr>>" +
-		"<'row'<'col-sm-5'i><'col-sm-7'p>>",
-	renderer: 'bootstrap'
+		//"<'row'<'col-sm-6'l><'col-sm-6'f>>" +
+		//"<'row'<'col-sm-12'tr>>" +
+		//"<'row'<'col-sm-5'i><'col-sm-7'p>>"+
+		"rt<'bottom'iflp<'clear'>>",
+	renderer: 'bootstrap',
+	pagingType:   "full_numbers",
+	sLoadingRecords: "正在加载数据...",
+	sZeroRecords: "暂无数据",
+	stateSave: true,
+	searching: false,
+	order: [],
+	serverSide: true,
+	ajax: function (data, callback, settings) {
+		console.log(DataTable.$("table"));
+        //封装请求参数
+        var param = {};
+        param.limit = data.length;//页面显示记录条数，在页面显示每页显示多少项的时候
+        param.start = data.start;//开始的记录序号
+        param.page = (data.start / data.length)+1;//当前页码
+        //ajax请求数据
+        $.ajax({
+            type: "GET",
+            url: "/hello/list",
+            cache: false,  //禁用缓存
+            data: param,  //传入组装的参数
+            dataType: "json",
+            success: function (result) {
+                //console.log(result);
+                //setTimeout仅为测试延迟效果
+                setTimeout(function () {
+                    //封装返回数据
+                    var returnData = {};
+                    returnData.draw = data.draw;//这里直接自行返回了draw计数器,应该由后台返回
+                    returnData.recordsTotal = result.total;//返回数据全部记录
+                    returnData.recordsFiltered = result.total;//后台不实现过滤功能，每次查询均视作全部结果
+                    returnData.data = result.data;//返回的数据列表
+                    //console.log(returnData);
+                    //调用DataTables提供的callback方法，代表数据已封装完成并传回DataTables进行渲染
+                    //此时的数据需确保正确无误，异常判断应在执行此回调前自行处理完毕
+                    callback(returnData);
+                }, 200);
+            }
+        });
+	},
+	language: {
+        "processing": "正在加载数据...",
+		"lengthMenu": "显示 _MENU_ 项结果",
+		"zeroRecords": "暂无数据",
+		"info": "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
+		"infoEmpty": "显示第 0 至 0 项结果，共 0 项",
+		"infoFiltered": "(由 _MAX_ 项结果过滤)",
+		"infoPostFix": "",
+		"url": "",
+		"paginate": {
+			"first":    "首页",
+			"previous": "上一页",
+			"next":     "下一页",
+			"last":     "末页"
+		}
+   }
 } );
 
 
